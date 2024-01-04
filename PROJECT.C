bfcfe73d51b63372b8df463a50330091f8ea2850
dharmi9397 <patelzerox2008@gemail.com>
2024-01-04 16:51:42 +0530

    project1

---
+++ PROJECT.C
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <conio.h>
+main()
+{
+ int a;
+ clrscr();
+ printf("enter a any number :-");
+ scanf("%d",&a);
+ if(a%2==0){
+  printf("is even");
+  }else{
+  printf("is odd");
+ }
+ getch();
+
+}
